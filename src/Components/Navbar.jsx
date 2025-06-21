c7ec4458ce7c4a39335f25d3b6e8ef47c098149a
Nitin Patel <156987426+nitin-pat@users.noreply.github.com>
2025-06-21 06:58:35 +0530

    Fix styles/forms.css casing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link, useNavigate } from "react-router-dom";
-import "./navbar.css"
+import "../Components/navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
