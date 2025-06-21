0789f282af87b6682953adeaace76397d1ad0e9a
Nitin Patel <156987426+nitin-pat@users.noreply.github.com>
2025-06-21 07:16:13 +0530

    Fix forms.css import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./style/app.css"
+import "./styles/app.css"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
